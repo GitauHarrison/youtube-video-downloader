1be06ed62997ff29f7c2c31a4c1160f713c4e51e
harry <harrisongitau3@gmail.com>
2022-06-17 16:53:33 +0300

    Add functionality to download a video
    - Display link form in index page
    - Add functionality to download video when button in idex page is clicked
    - Display video information before downloading in downloads page
    - Download load video to user's local machine rather than the server or application (for storage purposes)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import Config
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
-from app import routes, errors
+bootstrap = Bootstrap(app)
+
+
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,35 @@
 from app import app
+from flask import render_template, request, redirect, url_for, flash, send_file, session
+from pytube import YouTube
+from io import BytesIO
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/", methods = ["GET", "POST"])
+@app.route("/index", methods = ["GET", "POST"])
 def index():
-    return "Hello, World!"
+    if request.method == "POST":
+        session['link'] = request.form.get('url')
+        print(session['link'])
+        try:
+            url = YouTube(session['link'])
+            url.check_availability()
+            print(url)
+            flash(f'Downloading {url.title}')
+        except:
+            flash('Error: invalid link')
+        return render_template("download.html", url = url)
+    return render_template("index.html")
+
+@app.route("/download", methods = ["GET", "POST"])
+def download():
+    if request.method == "POST":
+        buffer = BytesIO()
+        url = YouTube(session['link'])
+        print(url)
+        itag = request.form.get("itag")
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, attachment_filename=f'{url.title}.mp4',
+                        as_attachment=True, mimetype='video/mp4')
+    return redirect(url_for("index"))
