336c3e861a31493288d3ead6b51d39624f58e9af
harry <harrisongitau3@gmail.com>
2022-06-18 19:09:48 +0300

    Fix: Update the resolutions made available to user before downloading
    - adaptive=true: makes all resolutions available
    - file_extension='mp4' (or subtype='mp4'): selects only mp4 files
    - progressive=true: initially used to retrieve mp4 files of resolution 720 and below

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,10 @@ from io import BytesIO
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/index", methods = ["GET", "POST"])
 def index():
+    """
+    When the form is submitted, the video link is
+    parsed and made ready for download.
+    """
     if request.method == "POST":
         session['link'] = request.form.get('url')
         try:
@@ -21,16 +25,22 @@ def index():
                 video_length = str(hours) + ':' + str(minutes) + ':' + str(seconds)
                 return video_length
             video_duration = find_video_length()
-
+            resolution = url.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc()
             flash(f'Downloading {url.title}')
         except:
             flash('Error: invalid link or no link provided')
             return redirect(url_for('index'))
-        return render_template("download.html", url=url, video_duration=video_duration)
+        return render_template(
+            "download.html",
+            url=url,
+            video_duration=video_duration,
+            resolution=resolution)
     return render_template("index.html", title="Home")
+
 
 @app.route("/download", methods = ["GET", "POST"])
 def download():
+    """Downloads the video and saves it to the user's computer"""
     if request.method == "POST":
         buffer = BytesIO()
         url = YouTube(session['link'])
