983b192b906d6a2ee640279cdbe80c111f0a5e5d
harry <harrisongitau3@gmail.com>
2023-04-29 13:15:09 +0300

    Fix: Save logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,35 @@
 from flask import Flask
 from flask_moment import Moment
 from config import Config
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 moment = Moment(app)
+
+
+if not app.debug:
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/byvd.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('byvd startup')
 
 
 from app import routes, errors
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    LOG_TO_STDOUT = 1
